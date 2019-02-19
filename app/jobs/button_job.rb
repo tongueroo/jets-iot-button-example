54606f5e9e223d55e99c7a93212be2a10849ab8c
Tung Nguyen <tongueroo@gmail.com>
2019-02-19 23:14:26 +0000

    Revert "simple message"
    This reverts commit b35dd3d5728d1d98a2f177d192e3de8e540ee881.

--- app/jobs/button_job.rb
+++ app/jobs/button_job.rb
@@ -4,12 +4,14 @@ class ButtonJob < ApplicationJob
   iam_policy "sns"
   iot_event "SELECT * FROM '#{ENV['BUTTON_TOPIC']}'"
   def deliver
+    puts "event #{JSON.dump(event)}"
+
     phone_number = ENV['PHONE_NUMBER']
-    message = "Hello from your IoT Button"
+    message = "Hello from your IoT Button #{event['serialNumber']}. Here is the full event: #{JSON.dump(event)}"
     sns.publish(
       phone_number: phone_number,
       message: message,
     )
-    puts "SMS has been sent"
+    puts "SMS has been sent to #{phone_number}"
   end
 end
